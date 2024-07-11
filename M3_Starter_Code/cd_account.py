519b9b497ca2759c35a3c1a344befb01a3cb23c6
gordon-laraven <laraven.gordon@gmail.com>
2024-07-11 14:40:05 -0400

    updated version

---
+++ M3_Starter_Code/cd_account.py
@@ -0,0 +1,33 @@
+"""Import the Account class from the Account.py file."""
+# ADD YOUR CODE HERE
+
+def create_cd_account(balance, interest_rate, months):
+    """Creates a CD account, calculates interest earned, and updates the account balance.
+
+    Args:
+        balance (float): The initial CD account balance.
+        interest_rate (float): The APR interest rate for the CD account.
+        months (int): The length of months for the CD.
+
+    Returns:
+        float: The updated CD account balance after adding the interest earned.
+        And returns the interest earned.
+    """
+    # Create an instance of the `Account` class and pass in the balance and interest parameters.
+    #  Hint: You need to add the interest as a value, i.e, 0.
+    # ADD YOUR CODE HERE
+
+    # Calculate interest earned
+    # ADD YOUR CODE HERE
+
+    # Update the CD account balance by adding the interest earned
+    # ADD YOUR CODE HERE
+
+    # Pass the updated_balance to the set balance method using the instance of the CDAccount class.
+    # ADD YOUR CODE HERE
+
+    # Pass the interest_earned to the set interest method using the instance of the CDAccount class.
+    # ADD YOUR CODE HERE
+
+    # Return the updated balance and interest earned.
+    return  # ADD YOUR CODE HERE
---
+++ M3_Starter_Code/savings_account.py
@@ -0,0 +1,34 @@
+"""Import the Account class from the Account.py file."""
+# ADD YOUR CODE HERE
+
+# Define a function for the Savings Account
+def create_savings_account(balance, interest_rate, months):
+    """Creates a savings account, calculates interest earned, and updates the account balance.
+
+    Args:
+        balance (float): The initial savings account balance.
+        interest_rate (float): The APR interest rate for the savings account.
+        months (int): The length of months to determine the amount of interest.
+
+    Returns:
+        float: The updated savings account balance after adding the interest earned.
+        And returns the interest earned.
+    """
+    # Create an instance of the `Account` class and pass in the balance and interest parameters.
+    #  Hint: You need to add the interest as a value, i.e, 0.
+    # ADD YOUR CODE HERE
+
+    # Calculate interest earned
+     # ADD YOUR CODE HERE
+
+    # Update the savings account balance by adding the interest earned
+    # ADD YOUR CODE HERE
+
+    # Pass the updated_balance to the set balance method using the instance of the SavingsAccount class.
+    # ADD YOUR CODE HERE
+
+    # Pass the interest_earned to the set interest method using the instance of the SavingsAccount class.
+    # ADD YOUR CODE HERE
+
+    # Return the updated balance and interest earned.
+    return  # ADD YOUR CODE HERE
